59bbe8f45ddbdc66c418f6c91e137e46ef389dd1
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-04-07 20:40:26 +0530

    Initialize the project

---
+++ gym_ceylon/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+          <span style={{color: 'white'}}>Koshika Dulanjana</span>
+    </div>
+  );
+}
+
+export default App;
