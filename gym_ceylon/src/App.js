516b027ef0446be67b028659a3aace9b754a2d5c
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-04-10 13:37:02 +0530

    updated

--- gym_ceylon/src/App.js
+++ gym_ceylon/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 function App() {
@@ -8,6 +9,7 @@ function App() {
           <Hero />
           <Programs />
           <Reasons />
+          <Plans />
     </div>
   );
 }
