8267214e228a30f9ab10d2aa18ec100319284f17
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-04-14 08:43:30 +0530

    updated

--- gym_ceylon/src/components/Hero/Hero.jsx
+++ gym_ceylon/src/components/Hero/Hero.jsx
@@ -9,6 +9,7 @@ import Calories from "../../assets/calories.png";
 const Hero = () => { 
   return (
     <div className='hero'>
+      <div className='blur hero-blur'></div>
         <div className='left-h'>
 
             <Header />
